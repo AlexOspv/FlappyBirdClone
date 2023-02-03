3a7c120db1acedfcf9fbcd817b0eeadb8e300ea1
AlexOspv <lesha_osipov@mail.ru>
2023-02-03 09:52:21 +0300

    Background and bird added

--- core/src/com/alexos/flappybirdclone/FlappyBird.java
+++ core/src/com/alexos/flappybirdclone/FlappyBird.java
@@ -1,31 +1,34 @@
 package com.alexos.flappybirdclone;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
+	Texture background;
+	Texture birdWingsUp;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
+		birdWingsUp = new Texture("bird_wings_up.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(birdWingsUp, Gdx.graphics.getWidth() / 2
+				- birdWingsUp.getWidth() / 2,
+				Gdx.graphics.getHeight() / 2
+				- birdWingsUp.getHeight() / 2);
 		batch.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
